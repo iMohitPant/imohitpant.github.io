58ed88045539ca85dc5c9c826df830b67ee18e75
Mohit Pant <74315540+iMohitPant@users.noreply.github.com>
2021-07-03 20:54:55 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -143,7 +143,7 @@ let footer = $(`
     
 
     <div class="rounded-social-buttons tag">
-    <a class="social-button twitter" href="#" target="_blank">
+    <a class="social-button twitter" href="https://twitter.com/iMohitPant" target="_blank">
     <!-- SVG code for twitter icon -->
     <svg class="twitter-icon-footer" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 500 500" enable-background="new 0 0 500 500"
